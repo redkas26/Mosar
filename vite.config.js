a011582746019121e3aa22a669de969fff2ac67f
RED <redkas@MacBook-Pro-de-RED.local>
2025-02-08 21:56:54 +0100

    fix path image

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Mosar/', // Remplace par le nom de ton repository
   plugins: [react()],
 })
