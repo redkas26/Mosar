a10f324353262be1fff42522e215ca04ae4f24eb
RED <redkas@MacBook-Pro-de-RED.local>
2025-02-08 21:44:09 +0100

    fix vite.config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Mosar/', // Remplace par le nom de ton repository
   plugins: [react()],
 })
